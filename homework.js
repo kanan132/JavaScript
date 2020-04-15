a1b8d0db24c7db4ec1d18901d59b20aae9134116
kanan132 <63307356+kanan132@users.noreply.github.com>
2020-04-15 23:49:07 +0400

    Steak task

---
+++ homework.js
@@ -0,0 +1,13 @@
+var a=prompt("Do you eat steak?");
+if(a =="yes"){
+    var age=prompt("How old are you?");
+    if(age<21){
+        alert("you cannot eat steak");
+    }
+    else{
+        alert("here it is,this is your steak");
+    }
+}
+else{
+    alert("here it is,your fries and ketchup");
+}
